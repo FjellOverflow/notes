2a42fb5ab7f49b8a045878b51f34fe4720de9fa6
Yannick Spörl <yannickspoerl@protonmail.com>
2023-12-19 22:57:23 +0200

    Add page hero next to title

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -4,16 +4,26 @@ import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 function PageTitle({ fileData, cfg, displayClass }: QuartzComponentProps) {
   const title = cfg?.pageTitle ?? "Untitled Quartz"
   const baseDir = pathToRoot(fileData.slug!)
+
   return (
-    <h1 class={`page-title ${displayClass ?? ""}`}>
-      <a href={baseDir}>{title}</a>
-    </h1>
+    <div style="display: flex; justify-content: start; align-items: center;">
+      <img src={`https://${cfg.baseUrl}/static/icon.svg`} class="desktop-only page-hero" />
+      <h1 class={`page-title ${displayClass ?? ""}`}>
+        <a href={baseDir}>{title}</a>
+      </h1>
+    </div>
   )
 }
 
 PageTitle.css = `
 .page-title {
   margin: 0;
+  font-size: 3rem;
+}
+
+.page-hero {
+  width: 64px;
+  margin-right: 16px;
 }
 `
 
